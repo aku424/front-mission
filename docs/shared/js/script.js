bfc27348d6fb1f8d14f92d542675cff4ecc88deb
takashima-takuto <aku424tt@gmail.com>
2022-07-29 06:09:01 +0900

    inputタグのフォーカス機能調整

--- docs/shared/js/script.js
+++ docs/shared/js/script.js
@@ -1,8 +1,5 @@
 let input_wrap = document.querySelector(".m-form-list__item--file-label");
 let input_item = input_wrap.children[0];
-    console.log(input_item);
-    console.log(input_wrap);
-
     
 input_item.addEventListener('focus', ()=> {
     input_wrap.classList.add("__focus");
