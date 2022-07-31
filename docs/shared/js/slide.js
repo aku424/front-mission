77b2551de361cb48744a3dbd4fa4ae391c8e81cf
takashima-takuto <aku424tt@gmail.com>
2022-07-31 19:25:21 +0900

    swiper

--- docs/shared/js/slide.js
+++ docs/shared/js/slide.js
@@ -1,7 +1,15 @@
 const swiper = new Swiper(".swiper", {
     loop: true,
-    slidesPerView:2,
-    spaceBetween: 40,
+    slidesPerView: 2,
+    spaceBetween: 20,
+    
+    breakpoints: {
+        // 768px以上の場合
+        768: {
+            spaceBetween: 40,
+            slidesPerView: 3,
+        }
+    },
 
     navigation: {
         nextEl: '.swiper-button-next',
